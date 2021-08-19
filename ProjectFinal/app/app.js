64a2db5db166ecb8ceab9fb6ba945c5941e7ea19
mahdis4092 <mahdishuvo99@gmail.com>
2021-08-19 10:54:51 +0600

    c

--- ProjectFinal/app/app.js
+++ ProjectFinal/app/app.js
@@ -5,15 +5,9 @@ app.config(["$routeProvider","$locationProvider",function($routeProvider,$locati
 
     $routeProvider
     .when("/", {
-<<<<<<< HEAD
         templateUrl : "views/pages/emADD.html",
         controller:"emADD"
 
-=======
-        templateUrl : "views/pages/login.html",
-        controller:"login"
-        
->>>>>>> a2f7fc55d592417e0c73122ac65e813fce73ed2b
     })
 
     .when("/emADD", {
@@ -21,21 +15,15 @@ app.config(["$routeProvider","$locationProvider",function($routeProvider,$locati
         controller:"emADD"
 
     })
-<<<<<<< HEAD
-    .when("/CustomerAdd", {
-        templateUrl : "views/pages/CustomerAdd.html",
-        controller: "CustomerAdd"
-=======
 
     .when("/login", {
         templateUrl : "views/pages/login.html",
         controller:"login"
-        
+
     })
     .when("/demo", {
         templateUrl : "views/pages/demopage.html",
         controller: 'demo'
->>>>>>> a2f7fc55d592417e0c73122ac65e813fce73ed2b
     })
     .when("/demo2", {
         templateUrl : "views/pages/demo2.html",
